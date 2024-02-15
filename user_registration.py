5e3f647832c4b7447de9d7bd73dad468a2f7a177
jasonkennedy01 <jason.kennedy@my.jcu.edu.au>
2024-02-15 10:09:07 +1000

    Add functions to user_registration.py

--- user_registration.py
+++ user_registration.py
@@ -1,2 +1,25 @@
 """Program to accept users registration using a computer generated password or user
 entry and converting to a hash code."""
+
+
+def main():
+    pass
+
+
+def generate_random_password():
+    pass
+
+
+def get_user_password():
+    pass
+
+
+def convert_to_hash():
+    pass
+
+
+def save_to_file():
+    pass
+
+
+main()
